ec2d03d80b040cb7d74fd043ea6c6fb598cf32fe
antonio <antoniosalas1996@gmail.com>
2020-12-23 23:07:41 -0600

    Updates

---
+++ precache-manifest.e87dfe0ec80d7e22ffc1b5c9dc78245f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9b69b1137a5a410b06b3960379cdab",
+    "url": "/mi_primer_app_react/index.html"
+  },
+  {
+    "revision": "6341ac8ef2717c48acd4",
+    "url": "/mi_primer_app_react/static/css/main.fc3aad8b.chunk.css"
+  },
+  {
+    "revision": "aaf0c9d52470a16be0dc",
+    "url": "/mi_primer_app_react/static/js/2.179a7dff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mi_primer_app_react/static/js/2.179a7dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6341ac8ef2717c48acd4",
+    "url": "/mi_primer_app_react/static/js/main.0314f747.chunk.js"
+  },
+  {
+    "revision": "d1c8f821a2ed0d1c4552",
+    "url": "/mi_primer_app_react/static/js/runtime-main.bce43d12.js"
+  }
+]);
