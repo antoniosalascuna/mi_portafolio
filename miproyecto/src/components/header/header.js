2c7c8e0df4c6b26c5adf74653b8b2701bbbc7dd3
antonio <antoniosalas1996@gmail.com>
2020-12-25 18:06:25 -0600

    actulizaciones

--- miproyecto/src/components/header/header.js
+++ miproyecto/src/components/header/header.js
@@ -11,12 +11,12 @@ export default class header extends Component {
     <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
     <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
     <ul id="nav" className="nav">
-      <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-      <li><a className="smoothscroll" href="#about">About</a></li>
-      <li><a className="smoothscroll" href="#resume">Resume</a></li>
-      <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-      <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-      <li><a className="smoothscroll" href="#contact">Contact</a></li>
+      <li className="current"><a className="smoothscroll" href="#home">Inicio</a></li>
+      <li><a className="smoothscroll" href="#about">Sobre mi</a></li>
+      <li><a className="smoothscroll" href="#resume">Resumen</a></li>
+      <li><a className="smoothscroll" href="#portfolio">Trabajos</a></li>
+      <li><a className="smoothscroll" href="#testimonials">Testimonios</a></li>
+      <li><a className="smoothscroll" href="#contact">Contacto</a></li>
     </ul> {/* end #nav */}
   </nav> {/* end #nav-wrap */}
   <div className="row banner">
