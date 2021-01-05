750dd6789c9f94d97bb4b26467e78cd5cb262e02
antonio <antoniosalas1996@gmail.com>
2021-01-05 14:46:26 -0600

    Updates

--- precache-manifest.37bd6237393e44bd5d1b7d2497a53c11.js
+++ precache-manifest.37bd6237393e44bd5d1b7d2497a53c11.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "209fbb3b82558fb61a18dd6c2b6cc34d",
+    "revision": "5f960d5b247a08932be34b4f67e29332",
     "url": "/mi_portafolio/index.html"
   },
   {
-    "revision": "52c5c661b4e967ddb863",
+    "revision": "f7ed3f75b9123ec162e8",
     "url": "/mi_portafolio/static/css/main.fc3aad8b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mi_portafolio/static/js/2.179a7dff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "52c5c661b4e967ddb863",
-    "url": "/mi_portafolio/static/js/main.537fdc79.chunk.js"
+    "revision": "f7ed3f75b9123ec162e8",
+    "url": "/mi_portafolio/static/js/main.bb3758f1.chunk.js"
   },
   {
     "revision": "8e327a7c84382f003d2c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mi_portafolio/precache-manifest.5ce894a25cc7ccdc4b33d356fd807428.js"
+  "/mi_portafolio/precache-manifest.37bd6237393e44bd5d1b7d2497a53c11.js"
 );
 
 self.addEventListener('message', (event) => {
